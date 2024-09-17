ca0739ab6457e0beeecf305cdbbcdad164a68c8b
rbulanad <rbulanad@student.42nice.fr>
2024-09-17 16:35:11 +0200

    some [arsing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "main.hpp"
 
-int ft_stoi(std::string & s)
+void	Parser(std::string cmd)
 {
-    int i;
-    std::istringstream(s) >> i;
-    return i;
+	std::string msg;
+	std::cout << cmd << std::endl;
+	if (cmd[0] == '\n')
+	{
+		 msg = "Skip";
+		throw (msg);
+	}
+	if (cmd.find('\n') != std::string::npos && cmd.find('\r') != std::string::npos && cmd.find(' ') != std::string::npos)
+	{
+		msg = "Not a command";
+		throw (msg);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -41,16 +50,26 @@ int	main(int argc, char **argv)
 	int	new_fd = accept(fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 	if (new_fd < 0)
 		close(fd), std::cout << "ERROR: Failed accept." << std::endl, exit(1);
-	std::cout << "Connection accepted, waiting for authentification..." << std::endl;
-	const char *msg = "Password? ";
-	send (new_fd, msg, strlen(msg), 0);
+	//std::cout << "Connection accepted, waiting for authentification..." << std::endl;
+	//const char *msg = "Password? ";
+	//send (new_fd, msg, strlen(msg), 0);
 	char buffer[1024] = {0};
 	memset(buffer, 0, sizeof(buffer));
-	int receive = recv(new_fd, buffer, 1024, 0);
-	std::string clientPass(buffer, receive);
-	clientPass.erase(std::remove(clientPass.begin(), clientPass.end(), '\n'), clientPass.end());
+	while (1)
+	{
+		int receive = recv(new_fd, buffer, 1024, 0);
+		if (receive < 1)
+			break ;
+		std::string iencli(buffer, receive);
+		try { Parser(iencli); }
+		catch (std::string msg)
+		{
+			std::cerr << msg << std::endl;
+		}
+	}
+	/*clientPass.erase(std::remove(clientPass.begin(), clientPass.end(), '\n'), clientPass.end());
 	clientPass.erase(std::remove(clientPass.begin(), clientPass.end(), '\r'), clientPass.end());
-	if (clientPass != servPass)
+	f (clientPass != servPass)
 	{
 		msg = "Wrong password.";
 		send (new_fd, msg, strlen(msg), 0);
@@ -59,7 +78,7 @@ int	main(int argc, char **argv)
 	{
 		msg = "Good password.";
 		send (new_fd, msg, strlen(msg), 0);
-	}
+	}*/
 	close(new_fd);
 	close(fd);
 	return (0);
